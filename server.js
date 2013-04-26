1c1f14f2f1223e523f48c5e6f218d71a5eba1fad
Runnable <git@runnable.com>
2013-04-26 00:17:26 +0000

    initialize project repo

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
+console.log('Hello Kitty');
