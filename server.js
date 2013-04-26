9a8121434c2ceb5b2c92ec8b1f150194026de7ac
sundippatel <sundip@runnable.com>
2013-04-25 17:19:01 -0700

    asdjf;

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
-console.log('Hello Kitty');
